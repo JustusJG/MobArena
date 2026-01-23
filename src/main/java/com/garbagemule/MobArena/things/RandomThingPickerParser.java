79e545cb8a0d6ef3f835ddd021623d01bcef5087
Justus Güttler <jguettler@gunter.software>
2026-01-23 01:31:19 +0100

    Add weighted random thing picker.

--- src/main/java/com/garbagemule/MobArena/things/RandomThingPickerParser.java
+++ src/main/java/com/garbagemule/MobArena/things/RandomThingPickerParser.java
@@ -1,7 +1,8 @@
 package com.garbagemule.MobArena.things;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RandomThingPickerParser implements ThingPickerParser {
@@ -24,20 +25,72 @@ public class RandomThingPickerParser implements ThingPickerParser {
         }
 
         String inner = ParserUtil.extractBetween(s, '(', ')');
-        List<ThingPicker> pickers = ParserUtil.split(inner)
+        Map<ThingPicker, Integer> relativeWeights = new HashMap<>();
+        Map<ThingPicker, Integer> absoluteWeights = new HashMap<>();
+        List<ThingPicker> unweightedPickers = ParserUtil.split(inner)
             .stream()
             .map(String::trim)
-            .map(parser::parse)
+            .map((thingString) -> {
+                Pattern weightedThingPattern = Pattern.compile("(?<weight>[0-9]*)(?<absolute>%?):(?<thing>.*)");
+                Matcher weightedThingMatcher = weightedThingPattern.matcher(thingString);
+
+                if (!weightedThingMatcher.matches()) {
+                    ThingPicker defaultWeightedThing = parser.parse(thingString);
+                    relativeWeights.put(defaultWeightedThing, 1);
+                    return defaultWeightedThing;
+                }
+                ThingPicker weightedThing = parser.parse(weightedThingMatcher.group("thing"));
+
+                int weight = Integer.parseInt(weightedThingMatcher.group("weight"));
+                if (weightedThingMatcher.group("absolute").isEmpty()) {
+                    relativeWeights.put(weightedThing, weight);
+                } else {
+                    absoluteWeights.put(weightedThing, weight);
+                }
+
+                return weightedThing;
+            })
             .collect(Collectors.toList());
 
-        if (pickers.isEmpty()) {
-            throw new IllegalArgumentException("Nothing to pick from: " + s);
+        double absoluteSum = absoluteWeights.values().stream().mapToInt(Integer::intValue).sum();
+        if (absoluteSum > 100) {
+            throw new IllegalArgumentException("Sum of absolute weights can't be more then 100%.");
         }
-        if (pickers.size() == 1) {
-            return pickers.get(0);
+        double remainingProbability = 1.0 - (absoluteSum / 100.0);
+        int relativeSum = relativeWeights.values().stream().mapToInt(Integer::intValue).sum();
+        if (relativeSum == 0) {
+            remainingProbability = 0;
         }
 
-        return new RandomThingPicker(pickers, random);
+        if (unweightedPickers.isEmpty()) {
+            throw new IllegalArgumentException("Nothing to pick from: " + s);
+        }
+        if (unweightedPickers.size() == 1) {
+            return unweightedPickers.get(0);
+        }
+
+        List<ThingPicker> weightedPickers = new ArrayList<>();
+        for (Map.Entry<ThingPicker, Integer> entry : absoluteWeights.entrySet()) {
+            int weight = entry.getValue();
+            int count = Math.toIntExact(Math.round(weight * 100.0));
+            for (int i = 0; i < count; i++) {
+                weightedPickers.add(entry.getKey());
+            }
+        }
+
+        for (Map.Entry<ThingPicker, Integer> entry : relativeWeights.entrySet()) {
+            ThingPicker picker = entry.getKey();
+            int weight = entry.getValue();
+            double probability = (double) weight / relativeSum * remainingProbability;
+            int count = (int) Math.round(probability * 10000);
+            for (int i = 0; i < count; i++) {
+                weightedPickers.add(picker);
+            }
+        }
+
+        Collections.shuffle(weightedPickers);
+
+        return new RandomThingPicker(weightedPickers, random);
     }
 
 }
